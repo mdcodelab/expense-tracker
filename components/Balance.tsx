113caaa3f8905a33fded13c61b561e6c04d5712e
miha11-hash <d_mihaela@msn.com>
2024-06-30 00:01:12 +0300

    format number with comas

--- components/Balance.tsx
+++ components/Balance.tsx
@@ -1,11 +1,12 @@
 import getUserBalance from "@/app/actions/GetUserBalance";
+import formatNumberWithCommas from "@/lib/utils";
 
 async function Balance() {
     const {balance} = await getUserBalance();
   return (
     <>
       <h4>Your Balance</h4>
-      <h1>${balance ?? 0}</h1>
+      <h1>${formatNumberWithCommas(balance) ?? 0}</h1>
     </>
   )
 }
---
+++ lib/utils.ts
@@ -0,0 +1,12 @@
+function formatNumberWithCommas(number:number) {
+    // Convert the number to a string
+    const numStr = number.toString();
+    
+    // Use a regular expression to insert commas every 3 digits from the right
+    const formattedStr = numStr.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+    return formattedStr;
+}
+
+export default formatNumberWithCommas;
+
