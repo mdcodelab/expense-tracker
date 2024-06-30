b7f43c26486051c805b2c4cdf60b2d279d8257a2
miha11-hash <d_mihaela@msn.com>
2024-06-30 19:58:32 +0300

    corrected variable types

--- components/Balance.tsx
+++ components/Balance.tsx
@@ -1,14 +1,19 @@
-import {getUserBalance} from "@/app/actions/GetUserBalance";
+import { getUserBalance } from "@/app/actions/GetUserBalance";
 import formatNumberWithCommas from "@/lib/utils";
 
+interface BalanceData {
+    balance?: number; 
+}
+
 async function Balance() {
-    const {balance} = await getUserBalance();
-  return (
-    <>
-      <h4>Your Balance</h4>
-      <h1>${formatNumberWithCommas(balance) ?? 0}</h1>
-    </>
-  )
+    const { balance }: BalanceData = await getUserBalance();
+    
+    return (
+        <>
+            <h4>Your Balance</h4>
+            <h1>${formatNumberWithCommas(balance ?? 0)}</h1>
+        </>
+    );
 }
 
 export default Balance;
--- components/IncomeExpense.tsx
+++ components/IncomeExpense.tsx
@@ -1,22 +1,26 @@
 import { getIncomeExpense } from "@/app/actions/GetUserBalance";
 import formatNumberWithCommas from "@/lib/utils";
 
-
 const IncomeExpense = async () => {
-    const {income, expense}= await getIncomeExpense();
+  interface IncomeExpenseData {
+    income?: number;
+    expense?: number;
+  }
+
+  const { income, expense }: IncomeExpenseData = await getIncomeExpense();
   
-    return (
-      <div className='inc-exp-container'>
-        <div>
-          <h4>Income</h4>
-          <p className='money plus'>${formatNumberWithCommas(income.toFixed(2))}</p>
-        </div>
-        <div>
-          <h4>Expense</h4>
-          <p className='money minus'>${formatNumberWithCommas(expense.toFixed(2))}</p>
-        </div>
+  return (
+    <div className='inc-exp-container'>
+      <div>
+        <h4>Income</h4>
+        <p className='money plus'>${formatNumberWithCommas(income?.toFixed(2) ?? "0.00")}</p>
       </div>
-    );
-  };
-  
-  export default IncomeExpense;
+      <div>
+        <h4>Expense</h4>
+        <p className='money minus'>${formatNumberWithCommas(expense?.toFixed(2) ?? "0.00")}</p>
+      </div>
+    </div>
+  );
+};
+
+export default IncomeExpense;
